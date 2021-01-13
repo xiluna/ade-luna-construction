9ea0553a0b1c2acc513b4cd93420c24e89a4bf4b
xiluna <xilaluna2@gmail.com>
2021-01-12 16:00:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 app.run(debug=True)
